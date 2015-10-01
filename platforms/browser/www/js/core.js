4b65ad85a66b462beb1ca7fece8c60a1c413ad5d
Vinh Nhon <thevinh92@vinhnhon.local>
2015-10-01 11:14:25 +0700

    For push

--- platforms/browser/www/js/core.js
+++ platforms/browser/www/js/core.js
@@ -1,13 +1,22 @@
-var effortlessEnglishApp = angular.module('EffortlessEnglishApp', []);
+var effortlessEnglishApp = angular.module('EffortlessEnglishApp', ['ngRoute']);
 
-effortlessEnglishApp.controller('HomeController', ['$scope', function($scope) {
+effortlessEnglishApp.controller('HomeController', ['$scope', '$location', function($scope, $location) {
 	$scope.name = 'Huy';
+
+	$scope.navigate = function(view) {
+		$location.path(view);
+	}
 }]);
+
 
 effortlessEnglishApp.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 		.when('/', {
 			templateUrl: 'view/home.html',
+			controller: 'HomeController'
+		})
+		.when('/second', {
+			templateUrl: 'view/second.html',
 			controller: 'HomeController'
 		});
 }]);
